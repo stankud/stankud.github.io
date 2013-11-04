2b8e72a2d655f27ea572460e60c72520b658a0cf
skud <st4sik@gmail.com>
2013-11-04 09:06:17 -0500

    add comments

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,16 +3,16 @@ var app = app || {};
 $(function() {
 	// fade the page in
 	$('.container').fadeIn('slow');
-	// d3 skills
+	// skills slide in
 	d3.selectAll('.skill').transition()
 	.duration(1000)
 	.delay(function(d,i){ return i * 350; })
 	.style('margin-left', '0px');
-
+	// first name slide
 	d3.select('.name').transition()
 	.duration(2000)
 	.style('margin-left', '0px');
-
+	// last name slide
 	d3.select('.spacing').transition()
 	.duration(2000)
 	.style('margin', '0px 0px');
